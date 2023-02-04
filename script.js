d42c552a6cb5c4f68917cb1e4520a933967df56a
Eduardo-M-Martins <martins-dudu@hotmail.com>
2023-02-03 22:42:54 -0300

    en passant

--- script.js
+++ script.js
@@ -1,159 +1,171 @@
-// Get the chess board element and the turn indicator element
+// Get the chess board element and create a div element to show the turn indicator
 const chessBoard = document.getElementById("chessBoard");
 const turnIndicator = document.createElement("div");
 
-// Set the text content of the turn indicator to "Current turn: White" and add it to the body
+// Set the text content, color, and alignment for the turn indicator and add it to the body
 turnIndicator.textContent = "Current turn: White";
 turnIndicator.style.color = 'white';
 turnIndicator.style.textAlign = 'center';
 document.body.appendChild(turnIndicator);
+
+// Initialize the variables for the current player, history, and selected piece
 let currentPlayer = "white";
+let history = [];
+let selectedPiece = null;
 
-// Object to store the chess piece symbols for white and black
-const pieceSymbols = {
-    white: {
-        pawn: "\u2659",
-        knight: "\u2658",
-        bishop: "\u2657",
-        rook: "\u2656",
-        queen: "\u2655",
-        king: "\u2654"
-    },
-    black: {
-        pawn: "\u265F",
-        knight: "\u265E",
-        bishop: "\u265D",
-        rook: "\u265C",
-        queen: "\u265B",
-        king: "\u265A"
+// Define the Piece class
+class Piece {
+    constructor(color, unicode, type) {
+        this.color = color;
+        this.unicode = unicode;
+        this.type = type;
     }
-};
+}
 
-// Loop through 8 rows and 8 columns to create the chess board
+// Initialize the board with pieces and empty squares
+let board = [];
 for (let i = 0; i < 8; i++) {
-    const row = chessBoard.insertRow(); // create a new row
+    board[i] = [];
     for (let j = 0; j < 8; j++) {
-        const cell = row.insertCell(); // create a new cell
-        // Add the class "white" or "black" depending on the row and column index
-        cell.className = (i + j) % 2 === 0 ? "white" : "black";
-        cell.addEventListener("click", handleClick); // add a click event listener
-        // Place the starting pieces on the board
-        if (i === 1) cell.textContent = pieceSymbols.black.pawn;
-        else if (i === 6) cell.textContent = pieceSymbols.white.pawn;
+        if (i === 1) board[i][j] = new Piece("black", "\u265F", "pawn"); // Black pawn
+        else if (i === 6) board[i][j] = new Piece("white", "\u2659", "pawn"); // White pawn
         else if (i < 2) {
-            if (j === 0 || j === 7) cell.textContent = pieceSymbols.black.rook;
-            else if (j === 1 || j === 6) cell.textContent = pieceSymbols.black.knight;
-            else if (j === 2 || j === 5) cell.textContent = pieceSymbols.black.bishop;
-            else if (j === 3) cell.textContent = pieceSymbols.black.queen;
-            else cell.textContent = pieceSymbols.black.king;
+            if (j === 0 || j === 7) board[i][j] = new Piece("black", "\u265C", "rook"); // Black rook
+            else if (j === 1 || j === 6) board[i][j] = new Piece("black", "\u265E", "knight"); // Black knight
+            else if (j === 2 || j === 5) board[i][j] = new Piece("black", "\u265D", "bishop"); // Black bishop
+            else if (j === 3) board[i][j] = new Piece("black", "\u265B", "queen"); // Black queen
+            else if (j === 4) board[i][j] = new Piece("black", "\u265A", "king"); // Black king
         } else if (i > 5) {
-            if (j === 0 || j === 7) cell.textContent = pieceSymbols.white.rook;
-            else if (j === 1 || j === 6) cell.textContent = pieceSymbols.white.knight;
-            else if (j === 2 || j === 5) cell.textContent = pieceSymbols.white.bishop;
-            else if (j === 4) cell.textContent = pieceSymbols.white.queen;
-            else cell.textContent = pieceSymbols.white.king;
+            if (j === 0 || j === 7) board[i][j] = new Piece("white", "\u2656", "rook"); // White rook
+            else if (j === 1 || j === 6) board[i][j] = new Piece("white", "\u2658", "knight"); // White knight
+            else if (j === 2 || j === 5) board[i][j] = new Piece("white", "\u2657", "bishop"); // White bishop
+            else if (j === 4) board[i][j] = new Piece("white", "\u2655", "queen"); // White queen
+            else if (j === 3) board[i][j] = new Piece("white", "\u2654", "king"); // White king
+        } else board[i][j] = new Piece("", "", "");
+    }
+}
+updateBoard(board);
+
+// Function to update the board
+function updateBoard(board) {
+    let currentBoard = [];
+    for (let i = 0; i < 8; i++) {
+        currentBoard[i] = [];
+        for (let j = 0; j < 8; j++) {
+            currentBoard[i][j] = board[i][j];
+        }
+    }
+    history.push(currentBoard);
+
+    while (chessBoard.hasChildNodes()) {
+        chessBoard.removeChild(chessBoard.firstChild);
+    }
+    for (let i = 0; i < 8; i++) {
+        const row = chessBoard.insertRow(); // Create a new row
+        for (let j = 0; j < 8; j++) {
+            const cell = row.insertCell(); // Create a new cell
+            cell.className = (i + j) % 2 === 0 ? "white" : "black";
+            cell.addEventListener("click", handleClick); // Add a click event listener
+            cell.textContent = history[history.length - 1][i][j].unicode;
         }
     }
 }
-let selectedPiece = null;
 
 // Handle the click event
 function handleClick(event) {
-    const targetCell = event.target;
+    // Get the row and column of the clicked cell
+    const row = event.target.parentNode.rowIndex;
+    const col = event.target.cellIndex;
 
-    const isCurrentPlayerPiece = targetCell.textContent.includes(pieceSymbols[currentPlayer].pawn) ||
-        targetCell.textContent.includes(pieceSymbols[currentPlayer].knight) ||
-        targetCell.textContent.includes(pieceSymbols[currentPlayer].bishop) ||
-        targetCell.textContent.includes(pieceSymbols[currentPlayer].rook) ||
-        targetCell.textContent.includes(pieceSymbols[currentPlayer].queen) ||
-        targetCell.textContent.includes(pieceSymbols[currentPlayer].king);
-
-    if (isCurrentPlayerPiece) {
-        if (!selectedPiece) {
-            selectedPiece = targetCell;
-            return;
-        }
+    if (board[row][col].color === currentPlayer) {
+        selectedPiece = { row, col };
     } else {
         if (!selectedPiece) return;
-        if (movePiece(selectedPiece, targetCell)) {
-            currentPlayer = currentPlayer === "white" ? "black" : "white";
-            turnIndicator.textContent = `Current turn: ${currentPlayer[0].toUpperCase() + currentPlayer.slice(1)}`;
-        }
+        const targetPiece = { row, col };
+        movePiece(selectedPiece, targetPiece);
         selectedPiece = null;
+        updateBoard(board);
     }
 }
 
 function movePiece(selectedPiece, targetPiece) {
-    const selectedRow = selectedPiece.parentNode.rowIndex;
-    const selectedCol = selectedPiece.cellIndex;
-    const targetRow = targetPiece.parentNode.rowIndex;
-    const targetCol = targetPiece.cellIndex;
+    const [fromRow, fromCol] = [selectedPiece.row, selectedPiece.col];
+    const [toRow, toCol] = [targetPiece.row, targetPiece.col];
+    let move = false;
 
-    // check the selected piece
-    if (selectedPiece.textContent === pieceSymbols[currentPlayer].knight) {
-        return knightMove(selectedRow, selectedCol, targetRow, targetCol);
-    } else if (selectedPiece.textContent === pieceSymbols[currentPlayer].pawn) {
-        return pawnMove(selectedRow, selectedCol, targetRow, targetCol, currentPlayer);
-    } else if (selectedPiece.textContent === pieceSymbols[currentPlayer].bishop) {
-        return bishopMove(selectedRow, selectedCol, targetRow, targetCol);
-    } else if (selectedPiece.textContent === pieceSymbols[currentPlayer].rook) {
-        return rookMove(selectedRow, selectedCol, targetRow, targetCol);
-    } else if (selectedPiece.textContent === pieceSymbols[currentPlayer].queen) {
-        return queenMove(selectedRow, selectedCol, targetRow, targetCol);
-    } else if (selectedPiece.textContent === pieceSymbols[currentPlayer].king) {
-        return kingMove(selectedRow, selectedCol, targetRow, targetCol);
+    if (board[fromRow][fromCol].type === "pawn") pawnMove();
+    else if (board[fromRow][fromCol].type === "knight") knightMove();
+    else if (board[fromRow][fromCol].type === "bishop") bishopMove();
+    else if (board[fromRow][fromCol].type === "rook") rookMove();
+    else if (board[fromRow][fromCol].type === "queen") queenMove();
+    else if (board[fromRow][fromCol].type === "king") kingMove();
+
+    if (move) {
+        board[toRow][toCol] = board[fromRow][fromCol];
+        board[fromRow][fromCol] = new Piece("", "", "");
+        test();
+        currentPlayer = currentPlayer === "white" ? "black" : "white";
+        turnIndicator.textContent = `Current turn: ${currentPlayer}`;
     }
-    return false;
 
-    function knightMove(selectedRow, selectedCol, targetRow, targetCol) {
-        // check if the knight is moving to a square that is 2 rows and 1 column away, or 2 columns and 1 row away
-        if ((Math.abs(selectedRow - targetRow) === 2 && Math.abs(selectedCol - targetCol) === 1) ||
-            (Math.abs(selectedRow - targetRow) === 1 && Math.abs(selectedCol - targetCol) === 2)) {
-            targetPiece.textContent = selectedPiece.textContent;
-            selectedPiece.textContent = "";
-            return true;
+    function pawnMove() {
+        const direction = currentPlayer === "white" ? -1 : 1;
+        // First move
+        if ((fromRow + direction * 2 === toRow) && (fromCol === toCol)
+            && (fromRow === 1 || fromRow === 6)) {
+            if (!board[toRow][toCol].color && !board[toRow - direction][toCol].color)
+                move = true;
         }
-        return false;
-    }
-
-    function pawnMove(selectedRow, selectedCol, targetRow, targetCol, currentPlayer) {
-        if(currentPlayer === "white"){
-            if(selectedRow == 6 && (selectedRow-targetRow==1||(selectedRow-targetRow==2 )) && selectedCol==targetCol && targetPiece.textContent == ""){
-                targetPiece.textContent = selectedPiece.textContent;
-                selectedPiece.textContent = "";
-                return true;
-            } else if(selectedRow != 6 && selectedRow-targetRow==1 && selectedCol==targetCol && targetPiece.textContent == ""){
-                targetPiece.textContent = selectedPiece.textContent;
-                selectedPiece.textContent = "";
-                return true;
-            }
-        } else {
-            if(selectedRow == 1 && (targetRow-selectedRow==1||(targetRow-selectedRow==2 )) && selectedCol==targetCol && targetPiece.textContent == ""){
-                targetPiece.textContent = selectedPiece.textContent;
-                selectedPiece.textContent = "";
-                return true;
-            } else if(selectedRow != 1 && targetRow-selectedRow==1 && selectedCol==targetCol && targetPiece.textContent == ""){
-                targetPiece.textContent = selectedPiece.textContent;
-                selectedPiece.textContent = "";
-                return true;
-            }
+        // Normal move
+        else if ((fromRow + direction === toRow) && (fromCol === toCol)
+            && !board[toRow][fromCol].color) {
+            move = true;
         }
-        return false;
+        // Capture move
+        else if ((fromRow + direction === toRow) &&
+            (fromCol + 1 === toCol || fromCol - 1 === toCol) && board[toRow][toCol].color !== ""
+            && board[toRow][toCol].color !== currentPlayer) {
+            move = true;
+        }
+        // En passant move
+        else if ((fromRow === 3 || fromRow === 4) && (fromCol + 1 === toCol || fromCol - 1 === toCol)
+            && board[fromRow][toCol].type === "pawn" && board[fromRow][toCol].color !== currentPlayer
+            && (fromRow + direction === toRow) && (history[history.length - 2][toRow + direction][toCol].type == "pawn")
+            && (history[history.length - 1][toRow + direction][toCol].type == "")) {
+            move = true;
+            board[fromRow][toCol] = new Piece("", "", "");
+        }
     }
 
-    function bishopMove(selectedRow, selectedCol, targetRow, targetCol) {
+    function knightMove() {
+        if ((Math.abs(fromRow - toRow) === 2 && Math.abs(fromCol - toCol) === 1) ||
+            (Math.abs(fromRow - toRow) === 1 && Math.abs(fromCol - toCol) === 2))
+            move = true;
+    }
+
+    function bishopMove() {
+ 
+    }
+
+    function rookMove() {
 
     }
 
-    function rookMove(selectedRow, selectedCol, targetRow, targetCol) {
+    function queenMove() {
 
     }
 
-    function queenMove(selectedRow, selectedCol, targetRow, targetCol) {
+    function kingMove() {
 
     }
+}
 
-    function kingMove(selectedRow, selectedCol, targetRow, targetCol) {
-
+function test() {
+    for (let k = 0; k < history.length; k++) {
+        for (let i = 0; i < 8; i++) {
+            console.log(history[k][i][0].unicode + " " + history[k][i][1].unicode + " " + history[k][i][2].unicode + " " + history[k][i][3].unicode + " " + history[k][i][4].unicode + " " + history[k][i][5].unicode + " " + history[k][i][6].unicode + " " + history[k][i][7].unicode);
+        }
+        console.log(k + "  _____________________________________________________________");
     }
+    console.log(history.length + "  =========================================================================================");
 }
